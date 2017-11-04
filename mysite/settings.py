88d1752b3b5ca1d8e92dd93bd4fc1f0f48859d6d
mau <tervaluoto@gmail.com>
2017-11-04 09:43:12 +0200

    Revert "changed debug to false"
    This reverts commit 907a6c35e9c3b3da26cf9f8e5eeaf983e762b492.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=x5_0#kz2dm#mv_!(w(j8(86m+p5w4@82ub7t#=nxzw-^b(-@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'maumade.pythonanywhere.com']
 
